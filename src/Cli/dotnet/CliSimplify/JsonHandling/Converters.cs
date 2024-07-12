3658be5c1b0be58670be3ec092abc64d4ff0bda5
Michael Yanni <miyanni@microsoft.com>
2024-07-12 16:03:07 -0700

    Began adding logic for using a custom Json contract and converters.

---
+++ src/Cli/dotnet/CliSimplify/JsonHandling/Converters.cs
@@ -0,0 +1,38 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Text.Json.Serialization;
+
+namespace Microsoft.DotNet.Cli.CliSimplify.JsonHandling
+{
+    // Might need a factory pattern for overall conversion.
+    // https://learn.microsoft.com/en-us/dotnet/standard/serialization/system-text-json/converters-how-to?pivots=dotnet-8-0#sample-factory-pattern-converter
+    // Currently using: https://learn.microsoft.com/en-us/dotnet/standard/serialization/system-text-json/converters-how-to?pivots=dotnet-8-0#sample-basic-converter
+    internal class CliConverter<T>(CliAttributeBundle Bundle) : JsonConverter<T>
+    {
+        public override T Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            //TODO: Need to define Json structure for storing everything (meaning metadata), reading it, and then storing it back 
+
+            //DateTimeOffset.ParseExact(reader.GetString()!, "MM/dd/yyyy", CultureInfo.InvariantCulture);
+            return default;
+        }
+
+        public override void Write(Utf8JsonWriter writer, T dateTimeValue, JsonSerializerOptions options)
+        {
+            //TODO: Need to define Json structure for storing everything (meaning metadata) based on the attribute values in Bundle.
+            // Then, write the Json properties for those attributes (and the property for the value itself).
+
+            throw new Exception($"{Bundle.Name} is not supported.");
+
+            //writer.WriteStringValue(dateTimeValue.ToString("MM/dd/yyyy", CultureInfo.InvariantCulture));
+        }
+    }
+}
---
+++ src/Cli/dotnet/CliSimplify/JsonHandling/ModifierContract.cs
@@ -0,0 +1,46 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization.Metadata;
+using System.Threading.Tasks;
+
+namespace Microsoft.DotNet.Cli.CliSimplify.JsonHandling
+{
+    internal class ModifierContract
+    {
+        // https://learn.microsoft.com/en-us/dotnet/standard/serialization/system-text-json/custom-contracts#example-increment-a-propertys-value
+        public static void CliAttributeHandler(JsonTypeInfo typeInfo)
+        {
+            foreach (var propertyInfo in typeInfo.Properties)
+            {
+                var name = propertyInfo.GetAttributeOrDefault<CliNameAttribute>();
+                var alias = propertyInfo.GetAttributeOrDefault<CliAliasAttribute>();
+                var accessType = propertyInfo.GetAttributeOrDefault<CliAccessTypeAttribute>();
+                var required = propertyInfo.GetAttributeOrDefault<CliRequiredAttribute>();
+                var position = propertyInfo.GetAttributeOrDefault<CliPositionAttribute>();
+
+                var bundle = new CliAttributeBundle(name, alias, accessType, required, position);
+
+                // TODO: Might need to make the converter against object since we don't know the type statically.
+                //propertyInfo.PropertyType
+                //propertyInfo.CustomConverter = new CliConverter<propertyInfo.PropertyType>(bundle);
+            }
+        }
+    }
+
+    internal static class JsonPropertyInfoExtensions
+    {
+        public static T GetAttributeOrDefault<T>(this JsonPropertyInfo propertyInfo) where T : Attribute
+        {
+            var attributes = propertyInfo.AttributeProvider?.GetCustomAttributes(typeof(T), true) ?? [];
+            // TODO: This only works if one attribute is defined for the property. Consider changing?
+            return attributes.Length == 1 ? (T)attributes[0] : null;
+        }
+    }
+
+    internal record CliAttributeBundle(CliNameAttribute Name, CliAliasAttribute Alias, CliAccessTypeAttribute AccessType, CliRequiredAttribute Required, CliPositionAttribute Position);
+}
